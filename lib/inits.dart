b0042372dfd5d60a6152dcd5b741f7175bf84d70
buzafar <sc.newcompany@gmail.com>
2025-12-11 21:18:24 +0500

    first commit

--- lib/inits.dart
+++ lib/inits.dart
@@ -2,10 +2,11 @@
 // here we initialize all the services like supabase when the app is launched
 
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Inits {
   static Future initSupabase() async {
-    await Supabase.initialize(url: 'https://csktohchkexpgswyeiwi.supabase.co',
-      anonKey: 'sb_publishable_eTCHYeQRQw6YpfhTLYvHJQ_oavMb3qC',);
+    await Supabase.initialize(url: dotenv.env['SUPABASE_URL']!,
+      anonKey: dotenv.env['SUPABASE_ANON_KEY']!,);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_business_extra/inits.dart';
 import 'package:my_business_extra/my_material_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: '.env');
 
   await Inits.initSupabase();
 
